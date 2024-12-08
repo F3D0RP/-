aa6cbb68e1f10696b9bdfd0dc066ee2e2f963ed0
F3D0RP <fedor090808@gmail.com>
2024-12-08 20:51:43 +0300

    Изменение основного окна, создание класса доп виджета

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
 import sys
 import sqlite3
-from PyQt6 import QtWidgets, uic
 
+from PyQt6.QtWidgets import QTableWidgetItem
+from PyQt6 import QtWidgets, uic
+from PyQt6.QtWidgets import QWidget
+
+
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+
+    def update_result(self):
+        cur = self.con.cursor()
+        # Получили результат запроса, который ввели в текстовое поле
+        result = cur.execute("SELECT * FROM films WHERE id=?",
+                             (item_id := self.spinBox.text(),)).fetchall()
+        # Заполнили размеры таблицы
+        self.tableWidget.setRowCount(len(result))
+        # Если запись не нашлась, то не будем ничего делать
+        if not result:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        else:
+            self.statusBar().showMessage(f"Нашлась запись с id = {item_id}")
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        # Заполнили таблицу полученными элементами
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
 class CoffeeApp(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
